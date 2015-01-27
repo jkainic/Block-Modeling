e82a0606840d702f73ccedb2e15e64b54461f9c2
Jenna Kainic <jkainic@users.noreply.github.com>
2015-01-27 16:21:00 -0500

    Update blockmodel.py

--- blockmodel.py
+++ blockmodel.py
@@ -1,6 +1,27 @@
-from numpy import *
-import math
+from math import *
 from copy import *
+from numpy import random
+
+def sum2(mat2d):
+	total = 0
+	for row in mat2d:
+		for x in row:
+			total += x
+	return total
+
+def sum1(vec):
+	total = 0
+	for el in vec:
+		total += el
+	return total
+
+#make the stack
+def concatenate(matrix_list):
+	total_mat = []
+	for mat in matrix_list:
+		for row in mat:
+			total_mat.append(row)
+	return total_mat
 
 class block_mat:
 	def __init__(self, matrix_list, num_blocks = 1):
@@ -29,7 +50,7 @@ class block_mat:
 
 		self.nu = [0]*self.num_mats
 		for m in range(self.num_mats):
-			sum_nu = sum(self.matrix_list[m])
+			sum_nu = sum2(self.matrix_list[m])
 			x = sum_nu/float((self.ntwk_size*(self.ntwk_size-1)))
 			self.nu[m] = x
 
@@ -47,7 +68,7 @@ class block_mat:
 
 		self.nu = [0]*self.num_mats
 		for m in range(self.num_mats):
-			sum_nu = sum(self.matrix_list[m])
+			sum_nu = sum2(self.matrix_list[m])
 			x = sum_nu/float((self.ntwk_size*(self.ntwk_size-1)))
 			self.nu[m] = x
 
@@ -58,16 +79,8 @@ class block_mat:
 				self.index[i] = 0
 			s = pow(self.ntwk_size, 2) - self.ntwk_size
 			for m in range(self.num_mats):
-				d = float(sum(self.matrix_list[m]))/s
+				d = float(sum2(self.matrix_list[m]))/s
 				self.T += s*pow(d - self.nu[m], 2)
-
-	#make the stack
-	def concatenate(matrix_list):
-		total_mat = []
-		for mat in matrix_list:
-			for row in mat:
-				total_mat.append(row)
-		return total_mat
 
 	#run CONCOR algorithm to make a block model
 	def concor(self):
@@ -80,7 +93,7 @@ class block_mat:
 		#separate zero columns
 		for x in range(self.ntwk_size):
 			colx = [row[x] for row in total_mat]
-			if sum(colx) == 0:
+			if sum1(colx) == 0:
 				zeroblock.append(x)
 			else:
 				nonzeroblock.append(x)
@@ -260,65 +273,6 @@ class block_mat:
 
 		return moves
 
-	#same as optimization, but attempts to do so in fewer steps by focusing at each stage
-	#only on parts of the optimization function affected by the agent being considered...
-	#it is unclear at this time if this could actually be a nontrivial speed-up, because it appears
-	#that there would be a proportionate number of computations
-	def optimize_by_parts(self, f_diff):
-		moves = 0
-		prev_val = 0
-
-		while self.T > prev_val:
-			prev_val = self.T
-			i = 0
-			while i < self.ntwk_size:
-				j = 0
-				iblock = self.index[i]
-
-				while j < self.num_blocks:
-
-					if len(self.blocks[iblock]) == 1:
-						j = self.num_blocks
-
-					elif iblock == j:
-						j += 1
-
-					else:
-						n = f_diff(self.matrix_list, i, iblock, j, self.nu, self.smat, self.dmat, self.blocks)
-						if n > 0:
-							self.blocks[iblock].remove(i)
-							self.blocks[j].append(i)
-							self.index[i] = j
-							self.T += n
-
-							for k in range(self.num_blocks):
-								if k != iblock and k != j:
-									size = len(self.blocks[k])
-									self.smat[iblock][k] -= size 
-									self.smat[k][iblock] -= size
-									self.smat[j][k] += size
-									self.smat[k][j] += size
-
-								elif k == iblock:
-									self.smat[k][k] = pow(len(self.blocks[iblock]),2) - len(self.blocks[iblock])
-									self.smat[k][j] = len(self.blocks[j])*len(self.blocks[k])
-									self.smat[j][k] = len(self.blocks[j])*len(self.blocks[k])
-								else:
-									self.smat[k][k] = pow(len(self.blocks[j]),2) - len(self.blocks[j])
-
-							for m in range(self.num_mats):
-								self.dmat[m] = d_sum(self.matrix_list[m], self.blocks, self.smat)
-
-							moves += 1
-							j = self.num_blocks
-
-						else:
-							j += 1
-
-				i += 1
-
-		return moves
-
 #calculate T value based on a particular block model
 def T(nu, s_matrix, d_matrix):
 	T_val = 0
@@ -331,87 +285,6 @@ def T(nu, s_matrix, d_matrix):
 					T_val += s_matrix[i][j]*pow((d_matrix[m][i][j] - nu[m]),2)
 
 	return T_val
-	
-#calculate part of the T value affected by moving an agent from one block to another:
-#returns the difference... if positive, the move is beneficial
-def T_diff(matrix_list, agent, block1, block2, nu, s_matrix, d_matrix, blocks):
-	num_blocks = len(blocks)
-	old_part = 0
-	new_part = 0
-
-	for m in range(len(d_matrix)):
-		for i in range(num_blocks):
-			if i != block1 and i != block2:
-				size = len(blocks[i])
-				s_i1 = s_matrix[i][block1] - size
-				s_i2 = s_matrix[i][block2] + size
-
-				sum11 = 0
-				sum12 = 0
-				sum21 = 0
-				sum22 = 0
-				for j in blocks[block1]:
-					sum11 += matrix_list[m][agent][j]
-					sum12 += matrix_list[m][j][agent]
-				for j in blocks[block2]:
-					sum21 += matrix_list[m][agent][j]
-					sum22 += matrix_list[m][j][agent]
-
-				d_i1 = float(d_matrix[m][i][block1]*s_matrix[i][block1] - sum11) / s_i1
-				d_1i = float(d_matrix[m][i][block1]*s_matrix[i][block1] - sum12) / s_i1
-				d_i2 = float(d_matrix[m][i][block2]*s_matrix[i][block2] + sum21) / s_i2
-				d_2i = float(d_matrix[m][i][block2]*s_matrix[i][block2] + sum22) / s_i2
-
-				old_part += s_matrix[i][block1]*(pow((d_matrix[m][i][block1] - nu[m]), 2) + pow((d_matrix[m][block1][i] - nu[m]), 2))
-				old_part += s_matrix[i][block2]*(pow((d_matrix[m][i][block2] - nu[m]), 2) + pow((d_matrix[m][block2][i] - nu[m]), 2))
-
-				new_part += s_i1*(pow((d_i1 - nu[m]), 2) + pow((d_1i - nu[m]), 2))
-				new_part += s_i2*(pow((d_i2 - nu[m]), 2) + pow((d_2i - nu[m]), 2))
-
-			elif i == block1:
-				if len(blocks[block1]) > 1:
-					old_part += s_matrix[block1][block1]*pow((d_matrix[m][block1][block1] - nu[m]), 2)
-
-				if len(blocks[block1]) > 2:
-					s_ii = pow((len(blocks[block1]) - 1), 2) - (len(blocks[block1]) - 1)
-					sum_ii = 0
-					for j in blocks[i]:
-						sum_ii += matrix_list[m][agent][j] + matrix_list[m][j][agent]
-					d_ii = float(d_matrix[m][block1][block1]*s_matrix[block1][block1] - sum_ii) / s_ii
-
-					new_part += s_ii*pow((d_ii - nu[m]), 2)
-			else:
-				if len(blocks[block2]) > 1:
-					s_i1 = (len(blocks[block2]) + 1)*(len(blocks[block1]) - 1)
-
-				sub_i1 = 0
-				sub_1i = 0
-				add_i1 = 0
-				add_1i = 0
-				for j in blocks[block2]:
-					sub_i1 += matrix_list[m][agent][j]
-					sub_1i += matrix_list[m][j][agent]
-				for j in blocks[block1]:
-					add_i1 += matrix_list[m][agent][j]
-					add_1i += matrix_list[m][j][agent]
-				
-				d_i1 = float(d_matrix[m][block2][block1]*s_matrix[block2][block1] + add_i1 - sub_i1)/s_i1
-				d_1i =  float(d_matrix[m][block1][block2]*s_matrix[block1][block2] + add_1i - sub_1i)/s_i1
-
-				old_part += s_matrix[block2][block2]*pow((d_matrix[m][block2][block2] - nu[m]), 2)
-				old_part += s_matrix[block1][block2]*(pow((d_matrix[m][block1][block2] - nu[m]), 2) + pow((d_matrix[m][block2][block1] - nu[m]), 2))
-
-				if len(blocks[block2]) > 2:
-					s_ii = pow((len(blocks[block2]) + 1), 2) - (len(blocks[block2]) + 1)
-					sum_ii = 0
-					for j in blocks[i]:
-						sum_ii += matrix_list[m][agent][j] + matrix_list[m][j][agent]
-					d_ii = float(d_matrix[m][block2][block2]*s_matrix[block2][block2] + sum_ii) / s_ii
-					new_part += s_ii*pow((d_ii - nu[m]), 2)
-
-				new_part += s_i1*(pow((d_i1 - nu[m]), 2) + pow((d_1i - nu[m]), 2))
-
-	return new_part - old_part
 		
 #returns a matrix whose ij^th entry is the s value of blocks #i and #j
 def s_ij(blocking):
@@ -486,7 +359,7 @@ def split_block(mat, block):
 
 		for x in range(size):
 			for y in range(size):
-				if absolute(M_new[x][y]) < 0.99 and x != y:
+				if abs(M_new[x][y]) < 0.99 and x != y:
 					rep += 1
 					break
 
@@ -528,28 +401,3 @@ def pearsonr(x, y):
 		val = numerator / sqrt(xdenom * ydenom)
 
 	return val
-
-#given a particular block model, rearranges the columns and rows of matrices in the stack
-#so that agents in each block are adjacent to one another
-def blocked_mats(blocks, matrix_list, size):
-	blocked_mats = []
-	N = len(blocks)
-
-	for mat in matrix_list:
-		blocked_tie_mat = deepcopy(mat)
-
-		for x in range(N):
-			block1 = blocks[x]
-			n = len(block1)
-			for i in range(n):
-				s = block1[i]
-				for y in range(N):
-					block2 = blocks[y]
-					m = len(block2)
-					for j in range(m):
-						t = block2[j]
-						blocked_tie_mat[i][j] = mat[s][t]
-
-		blocked_mats.append(blocked_tie_mat)
-
-	return blocked_mats
